2f1fee58c866479ff02c35cf225dc77f49636944
plutphil <47215043+plutphil@users.noreply.github.com>
2021-05-29 19:34:33 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,5 +3,9 @@
 #include"JSON.hpp"
 int main()
 {
-	jsonfromfile("test.json");
+	auto json=jsonfromfile("test.json");
+	auto k = json.root->get("web-app")->get("servlet")->get(0)->get("servlet-name");
+	if (auto str = dynamic_cast<String*>(k)) {
+		printf("\nstring: '%s'\n", str->val.c_str());
+	}
 }
